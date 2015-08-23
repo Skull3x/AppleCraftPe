061ecccb2634e9cdc77040eaed53228de4516636
tigerza117 <noppawanza1171@gmail.com>
2015-08-23 19:27:13 +0700

    Update ptstart.php

--- PVPTEAM/src/tiger/main/ptstart.php
+++ PVPTEAM/src/tiger/main/ptstart.php
@@ -3,14 +3,23 @@
 namespace tiger\main;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
 
 class MyPlugin extends PluginBase{
 
     public function onEnable(){
         $this->getLogger()->info("pvpteam starting");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-
+    
+    public function onJoin(PlayerJoinEvent $event){
+		$player = $event->getPlayer();
+    }
+    
     public function onDisable(){
         $this->getLogger()->info("pvpteam stoping");
     }
+    
 }
