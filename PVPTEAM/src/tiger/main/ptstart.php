9e11283a7b5e7e63284e90797aed194e872f40b5
tigerza117 <noppawanza1171@gmail.com>
2015-08-23 19:11:43 +0700

    Update ptstart.php

--- PVPTEAM/src/tiger/main/ptstart.php
+++ PVPTEAM/src/tiger/main/ptstart.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace tiger\main;
+
+use pocketmine\plugin\PluginBase;
+
+class MyPlugin extends PluginBase{
+
+    public function onEnable(){
+        $this->getLogger()->info("pvpteam starting");
+    }
+
+    public function onDisable(){
+        $this->getLogger()->info("pvpteam stoping");
+    }
+}
